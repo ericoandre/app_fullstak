26081a95dd956d75f55c469e8b4a36c48c588f48
ericoandre <ericoandresilva@gmail.com>
2025-04-26 22:11:49 -0300

    'Init'

---
+++ imip-app/src/screens/SplashScreen/index.tsx
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export const SplashScreen = () => {
+  return (
+    <View>
+      <Text>ur appxxxxxx!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
